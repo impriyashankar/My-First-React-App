098229535f1724bc1f42c06a028fd37688734fd3
priya shankar <preiyah@gmail.com>
2022-03-25 20:21:44 +0100

    created basic page in react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import HelloWorld from './Components/HelloWorld'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <HelloWorld name = "Priya"/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
